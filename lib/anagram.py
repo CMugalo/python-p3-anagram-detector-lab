91c10bdbe7fd70c7857273624fabe5f97230ec73
Christian Mugalo <christian.mugalo@student.moringaschool.com>
2024-07-10 18:36:05 +0300

    anagram detector lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
 # your code goes here!
+class Anagram():
+    def __init__(self, word):
+        self.word = word
+        self.letters = sorted([letter for letter in word])
+    
+    def match(self, word_list):
+
+        matching_words = []
+
+        for word in word_list:
+            if self.letters == sorted([letter for letter in word]):
+                matching_words.append(word)
+            else:
+                return matching_words
